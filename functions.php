4ae088234ea150f16f4f62d1701ff85933312b27
VRConservation <vance.russell@outlook.com>
2025-06-26 18:33:47 +0100

    18 tags commit

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * eighteen-tags engine room
+ *
+ * @package eighteen-tags
+ * @developer shramee <shramee.srivastav@gmail.com>
+ */
+
+define( 'EIGHTEENTAGS_VERSION', '3.0.0' );
+
+/**
+ * Initialize all the things.
+ */
+
+require_once get_template_directory() . '/inc/init.php';
+
+/**
+ * Note: Do not add any custom code here. Please use a custom plugin so that your customizations aren't lost during updates.
+ * https://github.com/pootlepress/customisations
+ */
